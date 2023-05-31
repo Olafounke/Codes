1d03786027515633504f23535b45746224b452d6
ruth <123456789+olafounkebiaou@gmail.noreply.github.com>
2023-05-31 11:51:36 +0200

    Création du fichier et ajout du code Minimum Distances

---
+++ minimum_distances.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function minimumDistances($a) {
+    // Write your code here
+    
+    $d = 100000; // Initialisation au maximum possible
+    
+    // Deux boucles for pour comparer une valeur du tableau aux valeurs suivantes
+    for ( $i = 0; $i < count($a) ; $i++ ){ 
+        for( $j = $i + 1; $j < count($a); $j++){
+            
+            // Comparaison des valeurs
+            if( $a[ $i ] == $a[ $j ] && $i != $j ){
+                
+                //Determiner la distance minimum
+                $d = min( $d , abs ( $j - $i ) );
+                
+            }
+        }
+       
+    } 
+    // Si la distance minimum est toujours le maximum posible, il n'y a pas de correspondance 
+    if( $d == 100000 ) $d = -1;
+    
+     return $d;
+}
+
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = minimumDistances($a);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
