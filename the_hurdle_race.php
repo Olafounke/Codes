860d6c736e5b99efa5cfb55f4258ca79a72ee0d8
ruth <123456789+olafounkebiaou@gmail.noreply.github.com>
2023-06-01 15:14:17 +0200

    Suppression du code non nécessaire

--- the_hurdle_race.php
+++ the_hurdle_race.php
@@ -22,21 +22,3 @@ function hurdleRace($k, $height) {
    else return 0;
 
 }
-
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$n = intval($first_multiple_input[0]);
-
-$k = intval($first_multiple_input[1]);
-
-$height_temp = rtrim(fgets(STDIN));
-
-$height = array_map('intval', preg_split('/ /', $height_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$result = hurdleRace($k, $height);
-
-fwrite($fptr, $result . "\n");
-
-fclose($fptr);
